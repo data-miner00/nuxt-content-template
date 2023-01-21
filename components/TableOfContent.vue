856a0d319d3da1db702179326786929ed6829e9f
data-miner00 <data-miner00@users.noreply.github.com>
2023-01-22 02:00:30 +0800

    feat: Added toc

---
+++ components/TableOfContent.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+defineProps(["toc"]);
+</script>
+
+<template>
+  <aside
+    class="sticky top-[350px] bg-white font-bold rounded-lg border border-solid border-gray-200 xl:w-[272px] text-sm transform -translate-y-60"
+    aria-label="Table of Content"
+  >
+    <header class="py-3 px-7 border-b border-solid border-gray-200">
+      <h3 class="text-gray-600">In this article</h3>
+    </header>
+    <div class="py-3">
+      <div v-for="(item, index) in toc" :key="index">
+        <NuxtLink
+          class="py-2 block"
+          exact-active-class="bg-gray-50 text-green-600 pl-7"
+          :to="`#${item.id}`"
+          >{{ item.text }}</NuxtLink
+        >
+
+        <!-- Uncomment the code below to render secondary title -->
+        <!-- <div v-if="item.children">
+          <NuxtLink
+            v-for="(child, index) in item.children"
+            :key="index"
+            class="py-2 block"
+            exact-active-class="bg-gray-50 text-green-600 pl-10"
+            :to="`#${child.id}`"
+            >{{ child.text }}</NuxtLink
+          >
+        </div> -->
+      </div>
+    </div>
+  </aside>
+</template>
