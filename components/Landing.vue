7db050abf2b5260616f385934c51d90e6e233a2f
data-miner00 <data-miner00@users.noreply.github.com>
2023-01-21 23:24:51 +0800

    feat: Added landing component

---
+++ components/Landing.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="section pb-10 pt-32 lg:pt-52 lg:pb-28 relative">
+    <div class="lg:max-w-[75rem] mx-auto px-6">
+      <slot />
+    </div>
+  </section>
+</template>
+
+<style>
+.section::before {
+  content: "";
+  position: absolute;
+  inset: 0;
+  mask-image: linear-gradient(#000, #000, transparent);
+  background-color: #fafafa;
+  z-index: -1;
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E%3Cg fill='%239C92AC' fill-opacity='0.1'%3E%3Cpolygon fill-rule='evenodd' points='8 4 12 6 8 8 6 12 4 8 0 6 4 4 6 0 8 4'/%3E%3C/g%3E%3C/svg%3E");
+}
+</style>
