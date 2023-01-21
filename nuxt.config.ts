62f3b34dc7327a92ad0722bcea3f26e644440596
data-miner00 <data-miner00@users.noreply.github.com>
2023-01-22 00:42:03 +0800

    feat: Added head metadata

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,26 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1.0",
+      title: "Nuxt Content Template",
+      meta: [
+        {
+          name: "description",
+          content:
+            "A hand-crafted feature-rich document driven template powered by Nuxt and Nuxt Content.",
+        },
+      ],
+      link: [
+        {
+          rel: "icon",
+          type: "image/svg+xml",
+          href: "/nuxt.svg",
+        },
+      ],
+    },
+  },
   modules: ["@nuxt/content", "@nuxtjs/tailwindcss"],
   tailwindcss: {
     configPath: "./tailwind.config.js",
