4f4f9d0711046b5d362e7bfe4d1091bf2fdfe5b8
data-miner00 <data-miner00@users.noreply.github.com>
2023-01-22 02:02:14 +0800

    feat: Updated slug page

--- app.vue
+++ app.vue
@@ -1,10 +1,6 @@
-<script setup lang="ts">
-const counter = useState("counter", () => Math.round(Math.random() * 1000));
-</script>
-
 <template>
-  <div>
-    <div class="font-lg font-bold">{{ counter }}</div>
+  <div class="app">
+    <Header />
     <NuxtPage />
   </div>
 </template>
--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,57 @@
+<script setup lang="ts">
+const { path } = useRoute();
+
+const { data } = await useAsyncData(`content-${path}`, () => {
+  return queryContent(path).findOne();
+});
+
+const {
+  // Global references
+  globals,
+  navigation,
+  surround,
+  page,
+  // Computed properties from `page` key
+  excerpt,
+  toc,
+  type,
+  layout,
+  // Computed properties from `surround` key
+  next,
+  prev,
+} = useContent();
+
+useHead({
+  title: data.value?.title,
+});
+</script>
+
 <template>
-  <main class="prose">
-    <ContentDoc />
+  <Landing>
+    <span class="text-cyan-600 text-lg md:text-2xl font-bold mb-5">{{
+      data?.topic
+    }}</span>
+    <h1 class="text-5xl md:text-7xl font-bold mb-6 md:mb-10">
+      {{ data?.title }}
+    </h1>
+    <p
+      class="text-xl md:text-2xl font-normal text-gray-500 lg:max-w-[500px] xl:max-w-[700px]"
+    >
+      {{ data?.description }}
+    </p>
+  </Landing>
+  <main class="mx-auto flex justify-center">
+    <article
+      class="prose prose-stone max-w-[340px] sm:max-w-[65ch] prose-pre:bg-gray-100 prose-pre:border prose-pre:border-gray-200 prose-pre:border-solid prose-pre:rounded-lg prose-pre:text-slate-800"
+    >
+      <ContentRenderer :value="(data as Record<string, any> | undefined)">
+        <template #empty>
+          <p>no content found</p>
+        </template>
+      </ContentRenderer>
+    </article>
+    <div class="relative ml-10 hidden lg:block">
+      <TableOfContent :toc="toc.links" />
+    </div>
   </main>
 </template>
