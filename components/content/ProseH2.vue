2f2b0a8d530aec3829b45a7a2ff0bb453b231c51
data-miner00 <data-miner00@users.noreply.github.com>
2023-01-25 23:34:42 +0800

    feat: Add overridden prose h2 and h3

---
+++ components/content/ProseH2.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps<{ id: string }>();
+const heading = 2;
+const { anchorLinks } = useRuntimeConfig().public.content;
+const generate =
+  anchorLinks?.depth >= heading && !anchorLinks?.exclude.includes(heading);
+</script>
+
+<template>
+  <h2 :id="id" class="relative block">
+    <a
+      v-if="generate"
+      :href="`#${id}`"
+      class="absolute -left-6 top-2 px-1 bg-gray border border-solid border-gray-200 text-sm rounded no-underline text-gray-500"
+    >
+      #
+    </a>
+    <slot />
+  </h2>
+</template>
