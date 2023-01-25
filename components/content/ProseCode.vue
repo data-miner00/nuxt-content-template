7d90e506dd47fb2335a076c710e129ba9d74e84c
data-miner00 <data-miner00@users.noreply.github.com>
2023-01-25 23:04:29 +0800

    feat: Added customized ProseCode

---
+++ components/content/ProseCode.vue
@@ -0,0 +1,44 @@
+<script lang="ts">
+export default defineComponent({
+  props: {
+    code: {
+      type: String,
+      default: "",
+    },
+    language: {
+      type: String,
+      default: null,
+    },
+    filename: {
+      type: String,
+      default: null,
+    },
+    highlights: {
+      type: Array as () => number[],
+      default: () => [],
+    },
+    meta: {
+      type: String,
+      default: null,
+    },
+  },
+});
+</script>
+
+<template>
+  <div class="relative">
+    <span
+      v-if="filename"
+      class="block absolute py-1 px-2 text-xs rounded border border-solid border-gray-300 right-2 top-2 bg-gray-100"
+      >{{ filename }}</span
+    >
+    <slot />
+  </div>
+</template>
+
+<style>
+pre code .line {
+  display: block;
+  min-height: 1rem;
+}
+</style>
