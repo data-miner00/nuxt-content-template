56762aad54d3af22726b29b079d1b965bb5ab187
data-miner00 <data-miner00@users.noreply.github.com>
2023-02-26 19:00:43 +0800

    fix: Missing dark theme for 404 page

--- components/ContentNotFound.vue
+++ components/ContentNotFound.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <h1 class="text-5xl font-semibold text-center mb-20">
-      <span class="text-black">404 Not Found</span>
+      <span class="text-black dark:text-gray-50">404 Not Found</span>
     </h1>
-    <p class="text-lg text-gray-600 text-center">
+    <p class="text-lg text-gray-600 dark:text-gray-200 text-center">
       The content does not exist. Please try another one.
     </p>
   </div>
