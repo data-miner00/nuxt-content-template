3e9fc51bdd407d56c33fbc98494f6edea571475a
data-miner00 <data-miner00@users.noreply.github.com>
2023-04-08 17:37:59 +0800

    fix: Responsive caveats for mobile layout

--- components/ContentNotFound.vue
+++ components/ContentNotFound.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <h1 class="text-5xl font-semibold text-center mb-20">
+  <div class="py-20 md:py-0">
+    <h1 class="text-5xl font-semibold text-center mb-2 md:mb-20">
       <span class="text-black dark:text-gray-50">404 Not Found</span>
     </h1>
     <p class="text-lg text-gray-600 dark:text-gray-200 text-center">
